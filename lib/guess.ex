0b58593d3b9839e85a63c31bb9479eb8e190f623
Josia Almeida <josiaalmeida0001@gmail.com>
2022-10-15 00:20:35 +0100

    refatorando codigo

--- lib/guess.ex
+++ lib/guess.ex
@@ -2,6 +2,9 @@
 # Tudo que fica entre o do e end está dentro do mesmo escopo
 # def -> Serve para criar uma função
 
+# Podemos criar varias funções com o mesmo nome, porém as mais generica ficam no final
+# A regra de prioridade é igual ao css
+
 defmodule Guess do
   use Application
 
@@ -14,13 +17,33 @@ defmodule Guess do
     IO.puts("Let´s play guess the number")
 
     IO.gets("Pick a difficult level (1 - 3): ")
-    |> Integer.parse()
     |> parse_input()
+    |> pick_number()
     |> IO.inspect()
   end
+
+  def pick_number(level) do
+    level
+    |> get_range()
+    |> Enum.random()
+  end
+
   def parse_input(:error) do
     IO.puts("Invalid level!!")
     run()
   end
   def parse_input({num, _}), do: num
+  def parse_input(data) do
+    data|> Integer.parse()
+        |> parse_input()
+  end
+  def get_range(value) do
+    case value do
+      1 -> 1..10
+      2 -> 1..100
+      3 -> 1..1000
+      _ -> IO.puts("Please, choose a level valid")
+      run()
+    end
+  end
 end
