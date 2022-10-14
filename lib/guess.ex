1bb5e377a2161382289193aa8dd88fc037bb9a72
Josia Almeida <josiaalmeida0001@gmail.com>
2022-10-14 23:48:55 +0100

    Desistruturação em função

--- lib/guess.ex
+++ lib/guess.ex
@@ -1,18 +1,26 @@
+# defmodule é um modulo, o elixir trabalha com modulos
+# Tudo que fica entre o do e end está dentro do mesmo escopo
+# def -> Serve para criar uma função
+
 defmodule Guess do
-  @moduledoc """
-  Documentation for `Guess`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Guess.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(_,_) do
+    run()
+    {:ok, self()}
   end
+
+  def run do
+    IO.puts("Let´s play guess the number")
+
+    IO.gets("Pick a difficult level (1 - 3): ")
+    |> Integer.parse()
+    |> parse_input()
+    |> IO.inspect()
+  end
+  def parse_input(:error) do
+    IO.puts("Invalid level!!")
+    run()
+  end
+  def parse_input({num, _}), do: num
 end
